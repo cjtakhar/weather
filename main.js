9129fd9584cd15bcf44bb5a57ea0a15f6537b8bb
cj <55723814+cjtakhar@users.noreply.github.com>
2022-01-03 22:30:26 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,3 @@
+// OpenWeather Info //
+const openWeatherKey = 'b31c5a2a1e69ecb1f18be581122ff129';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}';
