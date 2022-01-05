281661c81ded37820a404699ef31d2bdfeff4ea5
cj <55723814+cjtakhar@users.noreply.github.com>
2022-01-04 16:19:01 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=San%20Francisco&appid=b31c5a2a1e69ecb1f18be581122ff129&units=imperial'
+  res.sendFile('/Users/kt/Documents/webdev/portfolio/Projects/Weather/index.html')
+});
+
+app.post("/", function(req, res) {
+    const query = req.body.cityName;
+    const apiKey = 'b31c5a2a1e69ecb1f18be581122ff129';
+    const units = 'imperial';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${units}&appid=${apiKey}`;
+    
+    
     https.get(url, function(response) {
         console.log(response.statusCode);
         response.on('data', function(data) {
@@ -14,17 +26,14 @@ app.get("/", function(req, res) {
             const icon = weatherData.weather[0].icon;
             const imageURL = `https://openweathermap.org/img/wn/${icon}@2x.png`
             res.write(`<p>The weather is currently ${description}.</p>`);
-            res.write(`<h1>The temperature in San Francisco is ${temp} degrees</h1>`);
+            res.write(`<h1>The temperature in ${query} is ${temp} degrees</h1>`);
             res.write(`<img src='${imageURL}'>`)
             res.send();
         })
     })
 })
 
+
 app.listen(3000, function() {
     console.log('Server is running on port 3000.')
 })
-
-// OpenWeather Info //
-const apiKey = 'b31c5a2a1e69ecb1f18be581122ff129';
-const weatherUrl = 'https://api.openweathermap.org/data/2.5/'
